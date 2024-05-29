8b568ac42347c1b977e8490db5b65ab583846f48
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-05-29 12:27:31 +0530

    Files and libraries setup

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+
+let players = {};
+let currentPlayer = "W";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+io.on("connection", (socket) => {
+  console.log("New user connected");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
