6036486a26bb53998c77fddc69b9a5b385e0b15c
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-05-29 13:03:03 +0530

    Backend setup

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const io = socket(server);
 const chess = new Chess();
 
 let players = {};
-let currentPlayer = "W";
+let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,6 +22,45 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("New user connected");
+
+  if (!players.white) {
+    players.white = socket.id;
+    socket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = socket.id;
+    socket.emit("playerRole", "b");
+  } else {
+    socket.emit("spectatorRole");
+  }
+
+  socket.on("disconnect", () => {
+    if (socket.id === players.white) {
+      delete players.white;
+    } else if (socket.id === players.black) {
+      delete players.black;
+    }
+  });
+
+  socket.on("move", (move) => {
+    try {
+      if (chess.turn === "w" && socket.id !== players.white) return;
+      if (chess.turn === "b" && socket.id !== players.black) return;
+
+      const result = chess.move(move);
+
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid Move");
+        socket.emit("invalidMove", move);
+      }
+    } catch (err) {
+      console.log(err);
+      socket.emit("Invalid Move: ", move);
+    }
+  });
 });
 
 server.listen(3000, () => {
